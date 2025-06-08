03c0b9722ca18e34ec2c74f89e420ebb863e54f5
shvveling <95494203+shvveling@users.noreply.github.com>
2025-06-09 01:12:53 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,17 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
-from handlers import start, services, orders, payments
+from handlers import start, services, orders
+from aiogram.types import BotCommand
+
+
+async def set_commands(bot: Bot):
+    commands = [
+        BotCommand(command="start", description="Botni ishga tushirish"),
+        BotCommand(command="menu", description="Asosiy menyu"),
+    ]
+    await bot.set_my_commands(commands)
+
 
 async def main():
     bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
@@ -11,11 +21,13 @@ async def main():
     start.register_handlers(dp)
     services.register_handlers(dp)
     orders.register_handlers(dp)
-    payments.register_handlers(dp)
 
-    print("Bot ishga tushmoqda...")
+    await set_commands(bot)
+
+    print("Umra Jet bot ishga tushmoqda...")
 
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
